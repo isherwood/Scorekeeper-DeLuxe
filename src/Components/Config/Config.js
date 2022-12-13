9b614d66644e9f5b3dcd59b9dd4dfd7844008c9c
Clint Buhs <clint@planetbuhs.net>
2022-12-13 16:28:58 -0600

    Converting inputs to uncontrolled

--- src/Components/Config/Config.js
+++ src/Components/Config/Config.js
@@ -7,7 +7,7 @@ import './styles.css';
 
 const Config = props => {
     const [name, setName] = useState('');
-    const [num, setNum] = useState('1');
+    const [num, setNum] = useState('');
 
     const nameInputRef = useRef(null);
     const numInputRef = useRef(null);
@@ -15,9 +15,9 @@ const Config = props => {
     const handleAddIncrement = () => {
         if (num && !props.increments.includes(num)) {
             props.addIncrement(num);
-        }
 
-        numInputRef.current.value = null;
+            numInputRef.current.value = '';
+        }
     }
 
     const handleRemoveIncrement = val => {
@@ -33,7 +33,8 @@ const Config = props => {
     const handleAddPlayer = () => {
         if (name && !props.players.filter(player => player.name === name).length) {
             props.addPlayer(name);
-            setName('');
+
+            nameInputRef.current.value = '';
             nameInputRef.current.focus();
         }
     }
@@ -54,9 +55,8 @@ const Config = props => {
                     label="Enter a number"
                 >
                     <Form.Control type="number" placeholder="Enter a number"
-                                  value={num}
-                                      ref={numInputRef}
-                                      onChange={event => setNum(event.target.value)}
+                                  ref={numInputRef}
+                                  onChange={event => setNum(event.target.value)}
                                   onKeyUp={handleIncrementInputKeyup}/>
                 </FloatingLabel>
 
@@ -68,9 +68,10 @@ const Config = props => {
             {props.increments.length > 0 && <p className='form-text m-0'>Click to remove.</p>}
 
             {props.increments.map(inc => (
-                <span className='badge bg-primary position-relative inc-badge fw-normal me-2 my-1 px-3 cursor-pointer overflow-hidden'
-                      key={inc}
-                      onClick={() => handleRemoveIncrement(inc)}>
+                <span
+                    className='badge bg-primary position-relative inc-badge fw-normal me-2 my-1 px-3 cursor-pointer overflow-hidden'
+                    key={inc}
+                    onClick={() => handleRemoveIncrement(inc)}>
                     <span className='lead'>{inc}</span>
                     <span className='lead bg-danger position-absolute start-0 top-0 w-100 h-100 d-none'>
                         <MdCancel color='white'/>
@@ -86,7 +87,6 @@ const Config = props => {
                     label="Enter a name"
                 >
                     <Form.Control type="text" placeholder="Enter a name"
-                                  value={name}
                                   ref={nameInputRef}
                                   onChange={event => setName(event.target.value)}
                                   onKeyUp={handlePlayerInputKeyup}/>
@@ -100,9 +100,10 @@ const Config = props => {
             {props.players.length > 0 && <p className='form-text m-0'>Click to remove.</p>}
 
             {props.players.map(player => (
-                <span className='badge bg-primary position-relative inc-badge fw-normal me-2 my-1 px-3 cursor-pointer overflow-hidden'
-                      key={player.name}
-                      onClick={() => props.removePlayer(player.name)}>
+                <span
+                    className='badge bg-primary position-relative inc-badge fw-normal me-2 my-1 px-3 cursor-pointer overflow-hidden'
+                    key={player.name}
+                    onClick={() => props.removePlayer(player.name)}>
                     <span className='lead'>{player.name}</span>
                     <span className='lead bg-danger position-absolute start-0 top-0 w-100 h-100 d-none'>
                         <MdCancel color='white'/>
