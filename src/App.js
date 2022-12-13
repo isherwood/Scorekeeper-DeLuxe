32f4aa9d7a10c9e0cd8b128210ad91b5a2a5bf16
Clint Buhs <clint@planetbuhs.net>
2022-12-13 16:26:58 -0600

    Configuration: increments and players

--- src/App.js
+++ src/App.js
@@ -1,10 +1,78 @@
 import './App.css';
-import {Container} from "react-bootstrap";
+import {Button, Col, Container, Offcanvas, Row} from 'react-bootstrap';
+import {MdSettings} from "react-icons/md";
+
+import Config from './Components/Config/Config';
+import {useState} from 'react';
 
 function App() {
+    const [showOffCanvas, setShowOffCanvas] = useState(true);
+    const [players, setPlayers] = useState([]);
+    const [increments, setIncrements] = useState([1, 5]);
+    // const [randomizeIncrement, setRandomizeIncrement] = useState(false);
+
+    const handleCloseOffCanvas = () => setShowOffCanvas(false);
+    const handleShowOffCanvas = () => setShowOffCanvas(true);
+
+    const handleAddPlayer = name => {
+        setPlayers(players => [...players, {name: name, scores: []}]);
+    }
+
+    const handleRemovePlayer = name => {
+        setPlayers(players.filter(player => player.name !== name));
+    }
+
+    const handleAddIncrement = inc => {
+        setIncrements(increments => [...increments, inc].sort(compareIntegers));
+    }
+
+    const handleRemoveIncrement = val => {
+        setIncrements(increments.filter((el) => el !== val));
+    }
+
+    const compareIntegers = (a, b) => {
+        return a - b;
+    }
+
     return (
         <Container fluid className='app-container d-flex flex-column vh-100'>
+            <Row className='align-items-center'>
+                <Col>
+                    <div className='text-center'>
+                        <h1 className='display-5 fst-italic text-primary mb-0 text-nowrap'>Scorekeeper DeLuxe</h1>
+                        <span className='text-nowrap text-muted'>A SeaBee Software Production</span>
+                    </div>
+                </Col>
 
+                <Col className='flex-grow-1 text-end'>
+                    <Button variant='link' size='lg' onClick={handleShowOffCanvas}>
+                        <MdSettings/>
+                    </Button>
+
+                    <Offcanvas placement='end' show={showOffCanvas} onHide={handleCloseOffCanvas}>
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title><h2 className='display-6'>Configuration</h2></Offcanvas.Title>
+                        </Offcanvas.Header>
+
+                        <Offcanvas.Body>
+                            <Config
+                                players={players}
+                                addPlayer={handleAddPlayer}
+                                removePlayer={handleRemovePlayer}
+                                increments={increments}
+                                addIncrement={handleAddIncrement}
+                                removeIncrement={handleRemoveIncrement}
+                                // randomizeIncrement={randomizeIncrement}
+                                // setRandomizeIncrement={val => setRandomizeIncrement(val)}
+                            />
+                        </Offcanvas.Body>
+                    </Offcanvas>
+                </Col>
+            </Row>
+
+            <Row className='flex-fill'>
+
+            </Row>
         </Container>
     );
 }
---
+++ src/Components/Config/Config.js
@@ -0,0 +1,116 @@
+import {Form, FloatingLabel, InputGroup, Button} from "react-bootstrap";
+import {useRef, useState} from "react";
+import {FaPlus, FaUserPlus} from "react-icons/fa";
+import {MdCancel} from "react-icons/md";
+
+import './styles.css';
+
+const Config = props => {
+    const [name, setName] = useState('');
+    const [num, setNum] = useState('1');
+
+    const nameInputRef = useRef(null);
+    const numInputRef = useRef(null);
+
+    const handleAddIncrement = () => {
+        if (num && !props.increments.includes(num)) {
+            props.addIncrement(num);
+        }
+
+        numInputRef.current.value = null;
+    }
+
+    const handleRemoveIncrement = val => {
+        props.removeIncrement(val);
+    }
+
+    const handleIncrementInputKeyup = event => {
+        if (event.key === 'Enter') {
+            handleAddIncrement();
+        }
+    }
+
+    const handleAddPlayer = () => {
+        if (name && !props.players.filter(player => player.name === name).length) {
+            props.addPlayer(name);
+            setName('');
+            nameInputRef.current.focus();
+        }
+    }
+
+    const handlePlayerInputKeyup = event => {
+        if (event.key === 'Enter') {
+            handleAddPlayer();
+        }
+    }
+
+    return (
+        <>
+            <h3 className='lead'>Score Increments</h3>
+
+            <InputGroup className="mb-3">
+                <FloatingLabel
+                    controlId="floatingInput"
+                    label="Enter a number"
+                >
+                    <Form.Control type="number" placeholder="Enter a number"
+                                  value={num}
+                                      ref={numInputRef}
+                                      onChange={event => setNum(event.target.value)}
+                                  onKeyUp={handleIncrementInputKeyup}/>
+                </FloatingLabel>
+
+                <Button variant='secondary' onClick={handleAddIncrement}>
+                    <FaPlus className='lead'/>
+                </Button>
+            </InputGroup>
+
+            {props.increments.length > 0 && <p className='form-text m-0'>Click to remove.</p>}
+
+            {props.increments.map(inc => (
+                <span className='badge bg-primary position-relative inc-badge fw-normal me-2 my-1 px-3 cursor-pointer'
+                      key={inc}
+                      onClick={() => handleRemoveIncrement(inc)}>
+                    <span className='lead'>{inc}</span>
+                    <span className='lead position-absolute start-0 top-0 w-100 h-100 d-none'>
+                        <MdCancel color='white'/>
+                    </span>
+                </span>
+            ))}
+
+            <h3 className='lead mt-4'>Players</h3>
+
+            <InputGroup className="mb-3">
+                <FloatingLabel
+                    controlId="floatingInput"
+                    label="Enter a name"
+                >
+                    <Form.Control type="text" placeholder="Enter a name"
+                                  value={name}
+                                  ref={nameInputRef}
+                                  onChange={event => setName(event.target.value)}
+                                  onKeyUp={handlePlayerInputKeyup}/>
+                </FloatingLabel>
+
+                <Button variant='secondary' onClick={handleAddPlayer}>
+                    <FaUserPlus className='lead'/>
+                </Button>
+            </InputGroup>
+
+            {props.players.length > 0 && <p className='form-text m-0'>Click to remove.</p>}
+
+            {props.players.map(player => (
+                <span className='badge bg-primary position-relative inc-badge fw-normal me-2 my-1 px-3 cursor-pointer'
+                      key={player.name}
+                      onClick={() => props.removePlayer(player.name)}>
+                <span className='lead'>{player.name}</span>
+                <span className='lead position-absolute start-0 top-0 w-100 h-100 d-none'>
+                    <MdCancel color='white'/>
+                    </span>
+                </span>
+            ))}
+        </>
+    )
+}
+
+export default Config;
